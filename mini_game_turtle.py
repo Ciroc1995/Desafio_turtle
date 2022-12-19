32a989595edbf6810c251952b373f6cb0b4e51cb
Cicero Cordeiro <cicero.cordeiro1995@gmail.com>
2022-12-19 14:50:08 -0300

    deixando_codigo_clean

--- mini_game_turtle.py
+++ mini_game_turtle.py
@@ -2,27 +2,35 @@ from turtle import Turtle
 
 t = Turtle()
 t.speed(1)
+
+def obter_distancia():
+    resposta = int(input('Quantos pixels deseja movimentar a tartaruga? '))
+    return resposta
+
+def rotacionar_turtle(turtle):
+    rotacao = input('Para que lado deseja rotacionar a tartaruga? Selecione "e" para esquerda ou "d" para direita ')
+    if rotacao == 'e':
+        rotacionar_para_esquerda(t)
+    elif rotacao == 'd':
+        rotacionar_para_direita(t)
+
+def rotacionar_para_esquerda(turtle):
+    angulo = int(input("Deseja rotacionar a tartaruga em quantos graus para esquerda? "))
+    t.left(angulo)
+
+def rotacionar_para_direita(turtle):
+    angulo = int(input("Deseja rotacionar a tartaruga em quantos graus para direita? "))
+    t.right(angulo)
+
 while True:
     direcao = input('Para onde deseja movimentar a tartaruga? Selecione "f" para frente ou "t" para trás ')
     if direcao == 'f':
-        distancia = int(input('Quantos pixels deseja movimentar a tartaruga? '))
-        rotacao = input('Para que lado deseja rotacionar a tartaruga? Selecione "e" para esquerda ou "d" para direita ')
-        if rotacao == 'e':
-            angulo = int(input("Deseja rotacionar a tartaruga em quantos graus para esquerda? "))
-            t.left(angulo)
-        elif rotacao == 'd':
-            angulo = int(input("Deseja rotacionar a tartaruga em quantos graus para direita? "))
-            t.right(angulo)
+        distancia = obter_distancia()
+        rotacionar_turtle(t)
         t.forward(distancia)
-    elif direcao == 't':
-        distancia = int(input('Quantos pixels deseja movimentar a tartaruga? '))
-        rotacao = input('Para que lado deseja rotacionar a tartaruga? Selecione "e" para esquerda ou "d" para direita ')
-        if rotacao == 'e':
-            angulo = int(input("Deseja rotacionar a tartaruga em quantos graus para esquerda? "))
-            t.left(angulo)
-        elif rotacao == 'd':
-            angulo = int(input("Deseja rotacionar a tartaruga em quantos graus para direita? "))
-            t.right(angulo)
+    if direcao == 't':
+        distancia = obter_distancia()
+        rotacionar_turtle(t)
         t.backward(distancia)
     resposta = input('Deseja continuar andando? "s/sim" ou "n/não" ')
     if resposta == 's':
